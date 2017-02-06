6848f428e6055d9ac40b0d27d4bb22240df9a71f
Sajeev Ramasamy <thorion3006@gmail.com>
2017-02-06 12:46:17 +0530

    Assignment 19 jQuery Next and Previous Buttons extra credit: Completed

--- scripts/album.js
+++ scripts/album.js
@@ -83,43 +83,29 @@ var trackIndex = function(album, song) {
     return album.songs.indexOf(song);
 };
 
-var nextSong = function() {
-    var getLastSongNumber = function(index) {
+var skipSong = function (skipTo) {
+    if (skipTo === 'next') {
+        var getLastSongNumber = function(index) {
         return index == 0 ? currentAlbum.songs.length : index;
-    };
+        };
     
-    var currentSongIndex = trackIndex(currentAlbum, currentSongFromAlbum);
-    currentSongIndex++;
+        var currentSongIndex = trackIndex(currentAlbum, currentSongFromAlbum);
+        currentSongIndex++;
     
-    if (currentSongIndex >= currentAlbum.songs.length) {
+        if (currentSongIndex >= currentAlbum.songs.length) {
         currentSongIndex = 0;
-    }
-    
-    setSong(currentSongIndex+1);
-
-    $('.currently-playing .song-name').text(currentSongFromAlbum.title);
-    $('.currently-playing .artist-name').text(currentAlbum.artist);
-    $('.currently-playing .artist-song-mobile').text(currentSongFromAlbum.title + " - " + currentAlbum.title);
-    $('.main-controls .play-pause').html(playerBarPauseButton);
-    
-    var lastSongNumber = getLastSongNumber(currentSongIndex);
-    var $nextSongNumberCell = getSongNumberCell(currentlyPlayingSongNumber);
-    var $lastSongNumberCell = getSongNumberCell(lastSongNumber);
-    
-    $nextSongNumberCell.html(pauseButtonTemplate);
-    $lastSongNumberCell.html(lastSongNumber);
-};
-
-var previousSong = function() {
-    var getLastSongNumber = function(index) {
+        }
+    } else {
+        var getLastSongNumber = function(index) {
         return index == (currentAlbum.songs.length - 1) ? 1 : index + 2;
-    };
+        };
     
-    var currentSongIndex = trackIndex(currentAlbum, currentSongFromAlbum);
-    currentSongIndex--;
+        var currentSongIndex = trackIndex(currentAlbum, currentSongFromAlbum);
+        currentSongIndex--;
     
-    if (currentSongIndex < 0) {
+        if (currentSongIndex < 0) {
         currentSongIndex = currentAlbum.songs.length - 1;
+        }
     }
     
     setSong(currentSongIndex+1);
@@ -159,6 +145,6 @@ var $nextButton = $('.main-controls .next');
 
 $(document).ready(function() {
     setCurrentAlbum(albumPicasso);
-    $previousButton.click(previousSong);
-    $nextButton.click(nextSong);
+    $previousButton.click(function(){skipSong('previous');});
+    $nextButton.click(function(){skipSong('next');});
  });
