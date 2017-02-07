14a64f23713d5d72f6addf5915b866c421af585d
Sajeev Ramasamy <thorion3006@gmail.com>
2017-02-07 11:09:04 +0530

    Final revision

--- scripts/album.js
+++ scripts/album.js
@@ -239,7 +239,7 @@ var setupSeekBars = function () {
 };
 
 var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
-var pauseButtonTemplate = '<a class="album-son-button"><span class="ion-pause"></span></a>';
+var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
 var playerBarPlayButton = '<span class="ion-play"></span>';
 var playerBarPauseButton = '<span class="ion-pause"></span>';
 
